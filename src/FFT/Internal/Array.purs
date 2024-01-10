44482e10ce7b58b386280862bb6d2fb18247f4e2
James Leslie <jamesleslie@posteo.net>
2024-01-10 15:35:38 +0000

    Refactor and use FFT module as location where foreign imports are done

--- src/FFT/Internal/Array.purs
+++ src/FFT/Internal/Array.purs
@@ -4,5 +4,7 @@ import Data.Array.ST
 
 foreign import _new :: forall h a. Int -> ST h (STArray h a)
 
+-- | Create a mutable array that is unfilled. This is not as safe as
+-- | generating a filled one, but is faster.
 new :: forall h a. Int -> ST h (STArray h a)
 new = _new
